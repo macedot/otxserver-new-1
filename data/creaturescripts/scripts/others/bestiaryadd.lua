18b32e4fc25df9a421beba7dccda46e798790fc0
Erick Nunes <erickzenho@live.com>
2020-04-20 07:16:20 -0300

    Update bestiary

--- data/creaturescripts/scripts/others/bestiaryadd.lua
+++ data/creaturescripts/scripts/others/bestiaryadd.lua
@@ -2,7 +2,7 @@
 
 
 function onKill(player, creature, lastHit)
-    if not player:isPlayer() or not creature:isMonster() or creature:getMaster() or creature:isPlayer() or not player:getClient().os == CLIENTOS_NEW_WINDOWS then
+    if not player:isPlayer() or not creature:isMonster() or creature:getMaster() or creature:isPlayer() then
         return true
     end
 	player:addBestiaryKill(creature:getName())
